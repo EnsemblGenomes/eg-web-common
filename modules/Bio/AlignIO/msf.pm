570984bb03af8d5c8f3a1a7c903cf43167a1f788
Eugene Kulesha <ek@ebi.ac.uk>
2014-01-28 16:15:39 +0000

    added license

--- modules/Bio/AlignIO/msf.pm
+++ modules/Bio/AlignIO/msf.pm
@@ -1,3 +1,21 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
 # $Id: msf.pm,v 1.3 2011-01-05 17:20:30 ek3 Exp $
 #
 # BioPerl module for Bio::AlignIO::msf
@@ -8,10 +26,6 @@
 #
 #       and the SimpleAlign.pm module of Ewan Birney
 #
-# Copyright Peter Schattner
-#
-# You may distribute this module under the same terms as perl itself
-# _history
 # September 5, 2000
 # POD documentation - main docs before the code
 
--- modules/Bio/Species.pm
+++ modules/Bio/Species.pm
@@ -1,3 +1,21 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
 # $Id: Species.pm,v 1.1 2011-06-29 13:47:22 nl2 Exp $
 #
 # BioPerl module for Bio::Species
@@ -6,9 +24,6 @@
 #
 # Cared for by James Gilbert <jgrg@sanger.ac.uk>
 # Reimplemented by Sendu Bala <bix@sendu.me.uk>
-#
-# You may distribute this module under the same terms as perl itself
-
 # POD documentation - main docs before the code
 
 =head1 NAME
