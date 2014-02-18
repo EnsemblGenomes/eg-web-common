c03b227dedd3416f747bdaa3cec66a8824f065ba
Eugene Kulesha <ek@ebi.ac.uk>
2014-02-18 13:46:47 +0000

    need to exlude homoeologues from the list of retured mlss and set vega variable to 0 for wheat

--- modules/EnsEMBL/Web/ConfigPacker.pm
+++ modules/EnsEMBL/Web/ConfigPacker.pm
@@ -342,4 +342,242 @@ group by t.ontology_id, rt.name
   $dbh->disconnect();
 }
 
+
+## EG : need to exclude HOMOEOLOGUES as well as PARALOGUES otherwise too many method link species sets that prevents web site from starting
+sub _summarise_compara_db {
+  my ($self, $code, $db_name) = @_;
+  
+  my $dbh = $self->db_connect($db_name);
+  return unless $dbh;
+  
+  push @{$self->db_tree->{'compara_like_databases'}}, $db_name;
+
+  $self->_summarise_generic($db_name, $dbh);
+  
+## EG
+# added 
+# and ml.type not like "%HOMOEOLOGUES"
+  # See if there are any intraspecies alignments (ie a self compara)
+  my $intra_species_aref = $dbh->selectall_arrayref('
+    select mls.species_set_id, mls.method_link_species_set_id, count(*) as count
+      from method_link_species_set as mls, 
+        method_link as ml, species_set as ss, genome_db as gd 
+      where mls.species_set_id = ss.species_set_id
+        and ss.genome_db_id = gd.genome_db_id 
+        and mls.method_link_id = ml.method_link_id
+        and ml.type not like "%PARALOGUES"
+        and ml.type not like "%HOMOEOLOGUES"
+        and mls.source != "ucsc"
+      group by mls.method_link_species_set_id, mls.method_link_id
+      having count = 1
+  ');
+  
+  my (%intra_species, %intra_species_constraints);
+  $intra_species{$_->[0]}{$_->[1]} = 1 for @$intra_species_aref;
+  
+  # look at all the multiple alignments
+  ## We've done the DB hash...So lets get on with the multiple alignment hash;
+  my $res_aref = $dbh->selectall_arrayref('
+    select ml.class, ml.type, gd.name, mlss.name, mlss.method_link_species_set_id, ss.species_set_id
+      from method_link ml, 
+        method_link_species_set mlss, 
+        genome_db gd, species_set ss 
+      where mlss.method_link_id = ml.method_link_id and
+        mlss.species_set_id = ss.species_set_id and 
+        ss.genome_db_id = gd.genome_db_id and
+        (ml.class like "GenomicAlign%" or ml.class like "%.constrained_element" or ml.class = "ConservationScore.conservation_score")
+  ');
+  
+  my $constrained_elements = {};
+  my %valid_species = map { $_ => 1 } keys %{$self->full_tree};
+  # Check if contains a species not in vega - use to determine whether or not to run vega specific queries
+  my $vega = 1;
+  
+  foreach my $row (@$res_aref) { 
+    my ($class, $type, $species, $name, $id, $species_set_id) = ($row->[0], uc $row->[1], ucfirst $row->[2], $row->[3], $row->[4], $row->[5]);
+    my $key = 'ALIGNMENTS';
+    
+    if ($class =~ /ConservationScore/ || $type =~ /CONSERVATION_SCORE/) {
+      $key  = 'CONSERVATION_SCORES';
+      $name = 'Conservation scores';
+    } elsif ($class =~ /constrained_element/ || $type =~ /CONSTRAINED_ELEMENT/) {
+      $key = 'CONSTRAINED_ELEMENTS';
+      $constrained_elements->{$species_set_id} = $id;
+    } elsif ($type !~ /EPO_LOW_COVERAGE/ && ($class =~ /tree_alignment/ || $type  =~ /EPO/)) {
+      $self->db_tree->{$db_name}{$key}{$id}{'species'}{'ancestral_sequences'} = 1 unless exists $self->db_tree->{$db_name}{$key}{$id};
+    }
+    
+    $vega = 0 if $species eq 'Ailuropoda_melanoleuca';
+## EG : this has to be 1 for wheat
+    $vega = 0 if $species eq 'Triticum_aestivum';
+    
+    if ($intra_species{$species_set_id}) {
+      $intra_species_constraints{$species}{$_} = 1 for keys %{$intra_species{$species_set_id}};
+    }
+    
+    $species =~ tr/ /_/;
+   
+    $self->db_tree->{$db_name}{$key}{$id}{'id'}                = $id;
+    $self->db_tree->{$db_name}{$key}{$id}{'name'}              = $name;
+    $self->db_tree->{$db_name}{$key}{$id}{'type'}              = $type;
+    $self->db_tree->{$db_name}{$key}{$id}{'class'}             = $class;
+    $self->db_tree->{$db_name}{$key}{$id}{'species_set_id'}    = $species_set_id;
+    $self->db_tree->{$db_name}{$key}{$id}{'species'}{$species} = 1;
+  }
+  
+  foreach my $species_set_id (keys %$constrained_elements) {
+    my $constr_elem_id = $constrained_elements->{$species_set_id};
+    
+    foreach my $id (keys %{$self->db_tree->{$db_name}{'ALIGNMENTS'}}) {
+      $self->db_tree->{$db_name}{'ALIGNMENTS'}{$id}{'constrained_element'} = $constr_elem_id if $self->db_tree->{$db_name}{'ALIGNMENTS'}{$id}{'species_set_id'} == $species_set_id;
+    }
+  }
+
+  $res_aref = $dbh->selectall_arrayref('SELECT method_link_species_set_id, value FROM method_link_species_set_tag JOIN method_link_species_set USING (method_link_species_set_id) JOIN method_link USING (method_link_id) WHERE type LIKE "%CONSERVATION\_SCORE" AND tag = "msa_mlss_id"');
+  
+  foreach my $row (@$res_aref) {
+    my ($conservation_score_id, $alignment_id) = ($row->[0], $row->[1]);
+    
+    next unless $conservation_score_id;
+    
+    $self->db_tree->{$db_name}{'ALIGNMENTS'}{$alignment_id}{'conservation_score'} = $conservation_score_id;
+  }
+  
+  # if there are intraspecies alignments then get full details of genomic alignments, ie start and stop, constrained by a set defined above (or no constraint for all alignments)
+  $self->_summarise_compara_alignments($dbh, $db_name, $vega ? undef : \%intra_species_constraints) if scalar keys %intra_species_constraints;
+  
+  my %sections = (
+    ENSEMBL_ORTHOLOGUES => 'GENE',
+    HOMOLOGOUS_GENE     => 'GENE',
+    HOMOLOGOUS          => 'GENE',
+  );
+  
+  # We've done the DB hash... So lets get on with the DNA, SYNTENY and GENE hashes;
+  $res_aref = $dbh->selectall_arrayref('
+    select ml.type, gd1.name, gd2.name
+      from genome_db gd1, genome_db gd2, species_set ss1, species_set ss2,
+       method_link ml, method_link_species_set mls1,
+       method_link_species_set mls2
+     where mls1.method_link_species_set_id = mls2.method_link_species_set_id and
+       ml.method_link_id = mls1.method_link_id and
+       ml.method_link_id = mls2.method_link_id and
+       gd1.genome_db_id != gd2.genome_db_id and
+       mls1.species_set_id = ss1.species_set_id and
+       mls2.species_set_id = ss2.species_set_id and
+       ss1.genome_db_id = gd1.genome_db_id and
+       ss2.genome_db_id = gd2.genome_db_id
+  ');
+  
+  ## That's the end of the compara region munging!
+
+## EG
+# added 
+# and ml.type not like "%HOMOEOLOGUES"
+
+  my $res_aref_2 = $dbh->selectall_arrayref(qq{
+    select ml.type, gd.name, gd.name, count(*) as count
+      from method_link_species_set as mls, method_link as ml, species_set as ss, genome_db as gd 
+      where mls.species_set_id = ss.species_set_id and
+        ss.genome_db_id = gd.genome_db_id and
+        mls.method_link_id = ml.method_link_id and
+        ml.type not like '%PARALOGUES'
+        and ml.type not like "%HOMOEOLOGUES"
+      group by mls.method_link_species_set_id, mls.method_link_id
+      having count = 1
+  });
+  
+  push @$res_aref, $_ for @$res_aref_2;
+  
+  foreach my $row (@$res_aref) {
+    my ($species1, $species2) = (ucfirst $row->[1], ucfirst $row->[2]);
+    
+    $species1 =~ tr/ /_/;
+    $species2 =~ tr/ /_/;
+    
+    my $key = $sections{uc $row->[0]} || uc $row->[0];
+    
+    $self->db_tree->{$db_name}{$key}{$species1}{$species2} = $valid_species{$species2};
+  }             
+  
+  ###################################################################
+  ## Section for colouring and colapsing/hidding genes per species in the GeneTree View
+  # 1. Only use the species_sets that have a genetree_display tag
+  
+  $res_aref = $dbh->selectall_arrayref(q{SELECT species_set_id FROM species_set_tag WHERE tag = 'genetree_display'});
+  
+  foreach my $row (@$res_aref) {
+    # 2.1 For each set, get all the tags
+    my ($species_set_id) = @$row;
+    my $res_aref2 = $dbh->selectall_arrayref("SELECT tag, value FROM species_set_tag WHERE species_set_id = $species_set_id");
+    my $res;
+    
+    foreach my $row2 (@$res_aref2) {
+      my ($tag, $value) = @$row2;
+      $res->{$tag} = $value;
+    }
+    
+    my $name = $res->{'name'}; # 2.2 Get the name for this set (required)
+    
+    next unless $name; # Requires a name for the species_set
+    
+    # 2.3 Store the values
+    while (my ($key, $value) = each %$res) {
+      next if $key eq 'name';
+      $self->db_tree->{$db_name}{'SPECIES_SET'}{$name}{$key} = $value;
+    }
+
+    # 3. Get the genome_db_ids for each set
+    $res_aref2 = $dbh->selectall_arrayref("SELECT genome_db_id FROM species_set WHERE species_set_id = $species_set_id");
+    
+    push @{$self->db_tree->{$db_name}{'SPECIES_SET'}{$name}{'genome_db_ids'}}, $_->[0] for @$res_aref2;
+  }
+  
+  ## End section about colouring and colapsing/hidding gene in the GeneTree View
+  ###################################################################
+
+  ###################################################################
+  ## Section for storing the genome_db_ids <=> species_name
+  $res_aref = $dbh->selectall_arrayref('SELECT genome_db_id, name, assembly FROM genome_db WHERE assembly_default = 1');
+  
+  foreach my $row (@$res_aref) {
+    my ($genome_db_id, $species_name) = @$row;
+    
+    $species_name =~ tr/ /_/;
+    
+    $self->db_tree->{$db_name}{'GENOME_DB'}{$species_name} = $genome_db_id;
+    $self->db_tree->{$db_name}{'GENOME_DB'}{$genome_db_id} = $species_name;
+  }
+  ###################################################################
+  
+  ###################################################################
+  ## Section for storing the taxa properties
+  
+  # Default name is the scientific name
+  $res_aref = $dbh->selectall_arrayref(qq(SELECT DISTINCT taxon_id, name FROM ncbi_taxa_name JOIN gene_tree_node_tag ON taxon_id=value WHERE tag='lost_taxon_id' AND name_class='scientific name'));
+  foreach my $row (@$res_aref) {
+    my ($taxon_id, $taxon_name) = @$row;
+    $self->db_tree->{$db_name}{'TAXON_NAME'}{$taxon_id} = $taxon_name;
+  }
+
+  # Better name is the ensembl alias
+  $res_aref = $dbh->selectall_arrayref(qq(SELECT taxon_id, name FROM ncbi_taxa_name WHERE name_class='ensembl alias name'));
+  foreach my $row (@$res_aref) {
+    my ($taxon_id, $taxon_name) = @$row;
+    $self->db_tree->{$db_name}{'TAXON_NAME'}{$taxon_id} = $taxon_name;
+  }
+
+  # And the age of each ancestor
+  $res_aref = $dbh->selectall_arrayref(qq(SELECT taxon_id, name FROM ncbi_taxa_name WHERE name_class='ensembl timetree mya'));
+  foreach my $row (@$res_aref) {
+    my ($taxon_id, $taxon_mya) = @$row;
+    $self->db_tree->{$db_name}{'TAXON_MYA'}{$taxon_id} = $taxon_mya;
+  }
+
+
+  ###################################################################
+  
+  $dbh->disconnect;
+}
+
+
 1;
