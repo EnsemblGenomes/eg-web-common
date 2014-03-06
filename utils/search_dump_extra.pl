dcb6108253938f6e3c1ed1e7d00d88cf4cfcc3be
Nick Langridge <nicklangridge@hotmail.com>
2014-03-06 10:41:48 +0000

    Import scripts from CVS eg-plugins/utils and drop some old versions

---
+++ utils/search_dump_extra.pl
@@ -0,0 +1,221 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use Getopt::Long;
+use XML::Generator;
+use Data::Dumper;
+use HTML::Entities;
+
+BEGIN {
+  unshift @INC, "$Bin/../../conf";
+  unshift @INC, "$Bin/../../";
+  require SiteDefs;
+  unshift @INC, $_ for @SiteDefs::ENSEMBL_LIB_DIRS;
+  require EnsEMBL::Web::Hub; 
+}
+
+my $dir  = '.';
+my $index_list;
+
+GetOptions(
+  'dir=s'   => \$dir,
+  'index=s' => \$index_list
+);
+
+my @indices      = $index_list ? map {ucfirst} split(/,/, $index_list) : qw(Species Seqregion);
+my $hub          = EnsEMBL::Web::Hub->new;
+my $species_defs = $hub->species_defs;
+my @species      = $species_defs->valid_species;
+(my $division    = $species_defs->ENSEMBL_SITETYPE) =~ s/ //;
+my $genomic_unit = $species_defs->GENOMIC_UNIT;
+my $release      = $species_defs->SITE_RELEASE_VERSION;
+my %core_dbs     = map { $species_defs->get_config($_, 'databases')->{DATABASE_CORE}->{NAME} => 1 } (@species);
+my $dbh          = $hub->database('core', $species[0])->db_handle;
+my $file;
+
+my $dispatch = {
+  Species   => \&print_species,
+  Seqregion => \&print_seqregions,
+};
+
+foreach my $index (@indices) {
+  print "\n--- $index ---\n";
+  
+  my $filename = "$dir/${index}_$division.xml";
+  print "starting $filename\n";
+
+  open $file, '>' , $filename or die "Cannot open index file $filename: $!"; 
+  print_header();
+  $dispatch->{$index}->();
+  print_footer();
+  close $file;
+
+  print "wrote $filename\n";
+}
+
+exit;
+
+#------------------------------------------------------------------------------
+
+sub escape { return encode_entities($_[0], q{&<>"'\''}) }
+
+sub print_header {
+  print $file qq{<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE database [ <!ENTITY auml "&#228;">]>
+<database>
+  <name>$division</name>
+  <release>$release</release>
+  <entries>};
+}
+
+sub print_footer {
+  print $file qq{
+  </entries>
+</database>
+  }
+}
+
+sub print_species {
+   
+  my @meta_keys = qw(  
+    assembly.accession
+    assembly.default
+    assembly.name
+    species.common_name
+    species.display_name
+    species.division
+    species.production_name
+    species.scientific_name
+    species.taxonomy_id
+  );
+  
+  my @meta_keys_multi = qw(
+    species.alias
+    species.classification
+  );
+  
+  my $key_to_field_name = sub {
+    my $str = shift;
+    $str =~ s/^species\.//;
+    $str =~ s/\./_/;
+    return $str;
+  };
+  
+  foreach my $db_name (sort keys %core_dbs) {
+    print "$db_name\n";
+    
+    # prepare the data
+      
+    my ($collection) = $db_name =~ /^(.+)_collection/i;
+      
+    $dbh->do("use $db_name");
+    
+    my $meta = $dbh->selectall_hashref(
+      'SELECT species_id, meta_key, meta_value FROM meta WHERE meta_key IN ("' . join('", "', @meta_keys) . '") 
+       ORDER BY species_id, meta_key, meta_id DESC', 
+      ['species_id', 'meta_key']
+    );
+    
+    my $meta_multi = $dbh->selectall_hashref(
+      'SELECT species_id, meta_key, meta_value FROM meta WHERE meta_key IN ("' . join('", "', @meta_keys_multi) . '") 
+       ORDER BY species_id, meta_key, meta_id DESC', 
+       ['species_id', 'meta_key', 'meta_value']
+    );
+      
+    # add entries
+    
+    foreach my $species_id (keys %$meta) {
+      my $production_name = $meta->{$species_id}->{'species.production_name'}->{meta_value};   
+      my $display_name    = $meta->{$species_id}->{'species.display_name'}->{meta_value}; 
+      my $tax_id          = delete $meta->{$species_id}->{'species.taxonomy_id'}->{meta_value};   
+  
+      my $fields;
+      $fields .= qq{<field name="collection">$collection</field>\n} if $collection; 
+      $fields .= qq{<field name="genomic_unit">$genomic_unit</field>\n};  
+      
+      foreach my $key (@meta_keys) {
+        if (my $value = escape($meta->{$species_id}->{$key}->{meta_value})) {
+          my $name = $key_to_field_name->($key);
+          $fields .= qq{<field name="$name">$value</field>\n};  
+        }
+      }   
+      
+      foreach my $key (@meta_keys_multi) {
+        if (my $hash = $meta_multi->{$species_id}->{$key}) {
+          my $name = $key_to_field_name->($key);
+          foreach my $value (map {escape($_)} keys %$hash) {
+            $fields .= qq{<field name="$name">$value</field>\n};  
+          }
+        }
+      }   
+      
+      print $file qq{
+      <entry id="$production_name">
+        <name>$display_name</name>
+        <cross_references>
+          <ref dbname="ncbi_taxonomy_id" dbkey = "$tax_id" /> 
+        </cross_references>
+        <additional_fields>
+          $fields
+        </additional_fields>
+      </entry>};
+    }
+  }
+}
+
+sub print_seqregions {
+  
+  my $max_len = 100000;
+  
+  foreach my $db_name (sort keys %core_dbs) {
+    print "$db_name\n";
+      
+    $dbh->do("use $db_name");
+    
+    my %production_name = map {@$_} @{ $dbh->selectall_arrayref("SELECT species_id, meta_value FROM meta WHERE meta_key = 'species.production_name'") };
+    my %display_name    = map {@$_} @{ $dbh->selectall_arrayref("SELECT species_id, meta_value FROM meta WHERE meta_key = 'species.display_name'") };
+    my %tax_id          = map {@$_} @{ $dbh->selectall_arrayref("SELECT species_id, meta_value FROM meta WHERE meta_key = 'species.taxonomy_id'") };
+    
+    # get seq regions from top 2 levels - along with mapping to top level 
+    my $seq_regions = $dbh->selectall_arrayref(
+      "SELECT DISTINCT sr.name AS seq_region_name, sr.length, asm.name AS asm_seq_region_name, cmp.asm_start, cmp.asm_end, cs.name AS coord_system_name, cs.species_id
+       FROM seq_region sr JOIN coord_system cs USING (coord_system_id)
+       LEFT JOIN assembly cmp ON cmp.cmp_seq_region_id = sr.seq_region_id
+       LEFT JOIN seq_region asm ON asm.seq_region_id = cmp.asm_seq_region_id
+       LEFT JOIN seq_region_attrib sra ON sra.seq_region_id = asm.seq_region_id
+       LEFT JOIN attrib_type `at` USING(attrib_type_id) 
+       WHERE (at.name = 'Top Level' OR at.name IS NULL)
+       AND cs.name != 'chunk' AND cs.name != 'ignored'
+       AND FIND_IN_SET('default_version', cs.attrib)",
+       { Slice => {} }
+    ); 
+    
+    foreach my $sr (@$seq_regions) {
+      my $id = $sr->{species_id};
+      
+      my $location = $sr->{asm_seq_region_name} ?
+        sprintf('%s:%s-%s', $sr->{asm_seq_region_name}, $sr->{asm_start}, $sr->{asm_end} > $sr->{asm_start} + $max_len - 1 ? $sr->{asm_start} + $max_len - 1 : $sr->{asm_end}) :
+        sprintf('%s:%s-%s', $sr->{seq_region_name}, '1', $sr->{length} > $max_len ? $max_len : $sr->{length});
+      
+      print $file qq{
+      <entry id="$sr->{seq_region_name}">
+        <name>$sr->{seq_region_name}</name>
+        <cross_references>
+          <ref dbname="ncbi_taxonomy_id" dbkey = "$tax_id{$id}" /> 
+        </cross_references>
+        <additional_fields>
+          <field name="species">$display_name{$id}</field>
+          <field name="production_name">$production_name{$id}</field>
+          <field name="length">$sr->{length}</field>
+          <field name="location">$location</field>
+          <field name="coord_system">$sr->{coord_system_name}</field>
+          <field name="genomic_unit">$genomic_unit</field>
+        </additional_fields>
+      </entry>};
+    }
+  }
+}
+
+
---
+++ utils/update_userdata_dbs.pl
@@ -0,0 +1,121 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+use DBI;
+use Data::Dumper;
+use Getopt::Long;
+
+my ($uhost, $uport, $uuser, $upass); # userdata
+my ($chost, $cport, $cuser, $cpass); # core
+
+GetOptions(
+  "uhost=s" => \$uhost, "uport=s" => \$uport, "uuser=s" => \$uuser, "upass=s" => \$upass,
+  "chost=s" => \$chost, "cport=s" => \$cport, "cuser=s" => \$cuser, "cpass=s" => \$cpass,
+) || die "Invalid options\n";
+
+die "Please supply userdata mysql credentials: --uhost --uport --uuser [--upass]\n" if !(defined $uhost and defined $uport and defined $uuser);
+die "Please supply core mysql credentials: --chost --cport --cuser [--cpass]\n"     if !(defined $chost and defined $cport and defined $cuser);
+
+# global db handles
+
+my $udbh = DBI->connect("DBI:mysql:database=test;host=$uhost;port=$uport", $uuser, $upass);
+my $cdbh = DBI->connect("DBI:mysql:database=test;host=$chost;port=$cport", $cuser, $cpass);
+
+my $species_db;
+
+# get userdata dbs
+
+my $udbs = $udbh->selectcol_arrayref("show databases like '%_userdata'");
+
+foreach (@$udbs) {
+  next if /_collection_/;
+  my ($species) = split /_userdata/;
+  $species_db->{$species}->{userdata} = $_ ;
+} 
+
+# get core dbs
+
+my $cdbs = $cdbh->selectcol_arrayref("show databases like '%_core_%'");
+
+foreach (@$cdbs) {
+  next if /_collection_/;
+  my ($species) = split /_core_/;
+  $species_db->{$species}->{core} = $_ ;
+} 
+
+print "Checking for missing userdata dbs...\n";
+
+my @missing = sort grep { ! exists $species_db->{$_}->{userdata} } keys %$species_db;
+
+if (@missing) {
+  print "Creating missing userdata dbs...\n";
+  
+  my $core_db = $species_db->{$missing[0]}->{'core'};
+  `mysqldump --no-data --lock-tables=false -h $chost -P $cport -u $cuser $core_db > /tmp/core_schema.sql`;
+  
+  foreach my $species (@missing) {
+    create_userdata_db($species_db->{$species}->{core});
+    $species_db->{$species}->{userdata} = "${species}_userdata";
+  }
+}
+
+print "Checking for changed assemblies...\n";
+
+foreach my $species (sort keys %$species_db) { 
+  if (!$species_db->{$species}->{core}) {
+    print "ORPHANED - userdata db $species doesn't have matching core db\n";
+    next;
+  }
+  
+  #print "DB $species\n";
+  my $core_db     = $species_db->{$species}->{core};
+  my $userdata_db = $species_db->{$species}->{userdata};
+  
+  $cdbh->do("USE $core_db");
+  $udbh->do("USE $userdata_db");
+
+  my $species_ids = $udbh->selectcol_arrayref('SELECT DISTINCT species_id FROM meta WHERE species_id IS NOT NULL');
+  
+  foreach my $species_id (@$species_ids) {
+    my $sql = 'SELECT meta_value FROM meta WHERE species_id = ? AND meta_key = "assembly.name"';
+    my $old_assembly =  $udbh->selectrow_array($sql, undef, $species_id);
+    my $new_assembly =  $cdbh->selectrow_array($sql, undef, $species_id);
+    if ($old_assembly ne $new_assembly) {
+      print "ASSEMBLY MISMATCH - $species, old: '$old_assembly', new: '$new_assembly'\n" ;
+      #upgrade_assembly($species_id, $core_db, $userdata_db);
+    }
+  }
+}
+
+$udbh->disconnect;
+$cdbh->disconnect;
+
+#------------------------------------------------------------------------------
+
+sub create_userdata_db {
+  my ($core_db) = @_;
+  return unless $core_db;
+  my ($species, $rest ) = split /_core_/, $core_db;
+  my $user_db = $species ."_userdata";
+
+  warn "CREATING $user_db \n";
+  `mysqldump --single_transaction -h $chost -P $cport -u $cuser $core_db analysis meta meta_coord coord_system seq_region > /tmp/${user_db}.sql`;
+
+  # creating userdata database for a new species
+  $udbh->do("CREATE DATABASE IF NOT EXISTS $user_db");
+  $udbh->do("use $user_db") or die $udbh->errstr;
+  `mysql -h $uhost -P $uport -u $uuser --password=$upass $user_db < /tmp/core_schema.sql`; # add an error handler here
+  `mysql -h $uhost -P $uport -u $uuser --password=$upass $user_db < /tmp/${user_db}.sql`;  # add an error handler here
+
+  # check if tables exist
+  my $tables_exist = $udbh->selectcol_arrayref('show tables');
+  warn "ERROR: Table structure wasn't loaded into $user_db\n" if !@$tables_exist
+  #`rm /tmp/{$user_db}.sql`;
+}
+
+#sub upgrade_assembly {
+#  my ($species_id, $core_db, $userdata_db) = @_;
+#}
+
+
