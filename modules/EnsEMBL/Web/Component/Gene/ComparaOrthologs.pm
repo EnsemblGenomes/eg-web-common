4afa0d59d31c85a54488c7ce2ed88cc357d1e1de
Nick Langridge <nicklangridge@hotmail.com>
2014-04-15 11:31:08 +0100

    Add DNA sequences download link

--- modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -329,8 +329,12 @@ sub content {
       $hub->url({ action => 'Compara_Ortholog', function => 'Alignment' . ($cdb =~ /pan/ ? '_pan_compara' : ''), })   
     );
     $html .= sprintf(
-      ' &nbsp;|&nbsp; <a href="%s" target="_blank">Download protein sequences</a>', 
-      $hub->url({ action => 'Compara_Ortholog', function => 'PepSequence', '_format'=>'Text' }) 
+      ' &nbsp;|&nbsp; <a href="%s" target="_blank">Download all protein sequences</a>', 
+      $hub->url({ action => 'Compara_Ortholog', function => 'PepSequence', _format => 'Text' }) 
+    ) if $cdb !~ /pan/;
+    $html .= sprintf(
+      ' &nbsp;|&nbsp; <a href="%s" target="_blank">Download all DNA sequences</a>', 
+      $hub->url({ action => 'Compara_Ortholog', function => 'PepSequence', _format => 'Text', seq => 'cds' }) 
     ) if $cdb !~ /pan/;
     $html .= '</p>';
    }
