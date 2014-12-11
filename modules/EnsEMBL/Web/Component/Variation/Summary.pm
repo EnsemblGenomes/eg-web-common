69e6bffea4ccd95da26e13911074cc330a5fa068
Nick Langridge <nicklangridge@hotmail.com>
2014-12-11 10:56:26 +0000

    Remove code that uses study_variation table - it no longer exists

--- modules/EnsEMBL/Web/Component/Variation/Summary.pm
+++ modules/EnsEMBL/Web/Component/Variation/Summary.pm
@@ -36,7 +36,6 @@ sub content {
   my $summary_table      = $self->new_twocol(
     $self->variation_source,
     $self->alleles($feature_slice),
-    $self->study(),
     $self->location,
     $feature_slice ? $self->co_located($feature_slice) : (),
     #$self->validation_status,
@@ -48,32 +47,6 @@ sub content {
   );
 
   return sprintf qq{<div class="summary_panel">$info_box%s</div>}, $summary_table->render;
-}
-
-sub study {
-  my $self       = shift;
-  my $object     = $self->object;
-  my $study_name;
-
-  my $va= $self->object->Obj->{adaptor};
-  my $study_id = $va->get_study_ids_by_var_id($self->object->Obj->dbID);
-
-  my $sa = $self->hub->get_adaptor('get_StudyAdaptor', 'variation');
-  my $study = $sa->fetch_all_by_dbID_list($study_id);
-
-  return unless $study;
-
-  my @return = ();
-
-  foreach (0.. @$study - 1) {
-    my $study_link;
-    my $ext_ref = $study->[$_]->{external_reference};
-    $study_link = $self->hub->get_ExtURL_link("PMID: $ext_ref", 'EUROPE_PMC', $ext_ref) if $ext_ref =~ /^(\d+)$/;
-
-    my $study_line        = sprintf '<a href="%s" class="constant">%s</a>', $study->[$_]->{url}, $study->[$_]->{name};
-    push @return, $study_line.' | '.$study->[$_]->{description}.' ['.$study_link.']' if $study->[$_]->{description} && $study_link;
-  }
-  return @return ? ['Study', join ',<br><br>', @return] : ();
 }
 
 1;
