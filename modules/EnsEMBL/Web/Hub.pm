b1c8a6a0d627792c7448b8222bd2804239b3a89d
Nick Langridge <nicklangridge@hotmail.com>
2015-03-20 16:51:01 +0000

    Only use ATAC for plants

--- modules/EnsEMBL/Web/Hub.pm
+++ modules/EnsEMBL/Web/Hub.pm
@@ -70,7 +70,7 @@ sub intra_species_alignments {
     
 
     my @methods = qw(LASTZ_NET TRANSLATED_BLAT_NET TRANSLATED_BLAT BLASTZ_NET);
-    push @methods, 'ATAC' if $SiteDefs::ENSEMBL_SITETYPE =~ /plants/;
+    push @methods, 'ATAC' if $SiteDefs::ENSEMBL_SITETYPE =~ /plants/i;
 
     foreach my $method (qw(LASTZ_NET TRANSLATED_BLAT_NET TRANSLATED_BLAT BLASTZ_NET)) { 
       my $mlss = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs($method, [$genomedb], 1);
