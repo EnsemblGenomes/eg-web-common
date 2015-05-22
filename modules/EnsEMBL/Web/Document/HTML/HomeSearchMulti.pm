c146cb12e7599c69abf1c4fd7f3767ece9a1cc4e
Nick Langridge <nicklangridge@hotmail.com>
2015-05-22 11:28:22 +0100

    Update loaction of species page

--- modules/EnsEMBL/Web/Document/HTML/HomeSearchMulti.pm
+++ modules/EnsEMBL/Web/Document/HTML/HomeSearchMulti.pm
@@ -92,7 +92,7 @@ sub genome_search {
       <div id="species_list" class="js_panel">
         <input type="hidden" class="panel_type" value="SpeciesList" />
         <h3 class="first">Search for a genome</h3>
-        <form id="species_autocomplete_form" action="/info/about/species.html" style="margin-bottom:5px" method="get">
+        <form id="species_autocomplete_form" action="species.html" style="margin-bottom:5px" method="get">
           <div>
            <input name="search" type="text" id="species_autocomplete" class="ui-autocomplete-input inactive" style="width:95\%; margin: 0" title="Start typing the name of a genome..." value="Start typing the name of a genome...">
           </div>
