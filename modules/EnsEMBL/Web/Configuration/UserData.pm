73154646420b19ed6104453eca249d2fa1b6d30d
Nick Langridge <nicklangridge@hotmail.com>
2015-12-03 10:30:45 +0000

    Reinstate tools menu as we still need it for data slicer

--- modules/EnsEMBL/Web/Configuration/UserData.pm
+++ modules/EnsEMBL/Web/Configuration/UserData.pm
@@ -23,26 +23,26 @@ use strict;
 sub modify_tree {
   my $self = shift;
 
-  my $convert_menu = $self->get_node( 'Conversion' );
-  ## Slice file attachment
- $convert_menu->append(
-  $self->create_node( 'SelectSlice', "Data Slicer",
-   [qw(select_vcf EnsEMBL::Web::Component::UserData::SelectSlice)],
-    { 'availability' => 1 }
+  my $tools_menu = $self->create_submenu('Conversion', 'Online Tools');
+
+  # Add nodes for data slicer tool 
+
+  $tools_menu->append(
+    $self->create_node( 'SelectSlice', "Data Slicer",
+     [qw(select_vcf EnsEMBL::Web::Component::UserData::SelectSlice)], { 'availability' => 1 }
   ));
 
- $convert_menu->append(
-  $self->create_node( 'SliceFile', '',
-    [], { 'command' => 'EnsEMBL::Web::Command::UserData::SliceFile',
-    'availability' => 1, 'no_menu_entry' => 1 }
+  $tools_menu->append(
+    $self->create_node( 'SliceFile', '',
+      [], { 'command' => 'EnsEMBL::Web::Command::UserData::SliceFile', 'availability' => 1, 'no_menu_entry' => 1 }
   ));
 
- $convert_menu->append(
-  $self->create_node( 'SliceFeedback', '',
-   [qw(vcf_feedback EnsEMBL::Web::Component::UserData::SliceFeedback)],
-    { 'availability' => 1, 'no_menu_entry' => 1 }
+  $tools_menu->append(
+    $self->create_node( 'SliceFeedback', '',
+     [qw(vcf_feedback EnsEMBL::Web::Component::UserData::SliceFeedback)], { 'availability' => 1, 'no_menu_entry' => 1 }
   ));
 }
+
 
 
 1;
