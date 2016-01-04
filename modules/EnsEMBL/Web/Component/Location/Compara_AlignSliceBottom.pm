b12bbfacb17418adec8a4f742a8ca936a5a6768a
Nick Langridge <nicklangridge@hotmail.com>
2016-01-04 10:30:47 +0000

    ENSEMBL-4292 fix alignment image export

--- modules/EnsEMBL/Web/Component/Location/Compara_AlignSliceBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/Compara_AlignSliceBottom.pm
@@ -26,7 +26,7 @@ sub content {
   my $self         = shift;
   my $hub          = $self->hub;
   my $species_defs = $hub->species_defs;
-  my $object       = $self->object;
+  my $object       = $self->object || $self->hub->core_object('location');
   my $threshold    = 1000100 * ($species_defs->ENSEMBL_GENOME_SIZE || 1);
   my $align_params = $hub->param('align');
   my %options      = ( scores => $hub->param('opt_conservation_scores'), constrained => $hub->param('opt_constrained_elements') );
@@ -51,7 +51,7 @@ sub content {
   
   my $image_width     = $self->image_width;
   my $slice           = $object->slice;
-  my ($slices)        = $self->object->get_slices({
+  my ($slices)        = $object->get_slices({
                                               'slice' => $slice, 
                                               'align' => $align_params, 
                                               'species' => $primary_species
