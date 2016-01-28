e7bc0920a33c8d0ea4cf41c2db94877b3fd8563d
Sanjay Boddu <sboddu@ebi.ac.uk>
2016-01-28 12:00:10 +0000

    ENSEMBL-4097 - Add EG version of Ensembl's new footer

---
+++ modules/EnsEMBL/Web/Document/Element/FatFooter.pm
@@ -0,0 +1,89 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Document::Element::FatFooter;
+
+### Optional fat footer - site-specific, so see plugins 
+
+use strict;
+
+use base qw(EnsEMBL::Web::Document::Element);
+
+sub content {
+  my $species_defs = shift->species_defs;
+  my $sister_sites = '<p><a href="http://www.ensembl.org">Ensembl</a></p>';
+  my $html = '<hr /><div id="fat-footer">';
+
+  $html .= qq(
+              <div class="column-four left">
+                <h3>About Us</h3>
+                <p><a href="http://ensemblgenomes.org/info/about">About us</a></p>
+                <p><a href="http://ensemblgenomes.org/info/about/contact">Contact us</a></p>
+                <p><a href="http://ensemblgenomes.org/info/publications">Citing Ensembl Genomes</a></p>
+                <p><a href="http://www.ebi.ac.uk/about/privacy">Privacy policy</a></p>
+                <p><a href="http://ensemblgenomes.org/info/about/legal">Disclaimer</a></p>
+                <p><a href="http://www.ensemblgenomes.org/info/about/cookies">Cookies</a></p>
+                <p><a href="http://www.ebi.ac.uk/Information/termsofuse.html">EMBL-EBI Terms of use</a></p>
+              </div>
+  );
+
+
+ $html .= qq(
+              <div class="column-four left">
+                <h3>Get help</h3>
+                <p><a href="/info/website/">Using this website</a></p>
+                <p><a href="http://ensemblgenomes.org/info">Documentation</a></p>
+                <p><a href="/info/website/upload">Adding custom tracks</a></p>
+                <p><a href="/info/website/ftp/index.html">Downloading data</a></p>
+              </div>
+  );
+
+  
+
+  $sister_sites .= '<p><a href="http://bacteria.ensembl.org">Ensembl Bacteria</a></p>' if $species_defs->ENSEMBL_SERVERNAME ne 'bacteria.ensembl.org';
+  $sister_sites .= '<p><a href="http://fungi.ensembl.org">Ensembl Fungi</a></p>' if $species_defs->ENSEMBL_SERVERNAME ne 'fungi.ensembl.org';
+  $sister_sites .= '<p><a href="http://plants.ensembl.org">Ensembl Plants</a></p>' if $species_defs->ENSEMBL_SERVERNAME ne 'plants.ensembl.org';
+  $sister_sites .= '<p><a href="http://protists.ensembl.org">Ensembl Protists</a></p>' if $species_defs->ENSEMBL_SERVERNAME ne 'protists.ensembl.org';
+  $sister_sites .= '<p><a href="http://metazoa.ensembl.org">Ensembl Metazoa</a></p>' if $species_defs->ENSEMBL_SERVERNAME ne 'metazoa.ensembl.org';
+
+  $html .= qq(
+              <div class="column-four left">
+                <h3>Our sister sites</h3>
+                $sister_sites
+              </div>
+  );
+
+
+  $html .= qq(
+              <div class="column-four left">
+                <h3>Follow us</h3>
+                <p><a class="media-icon" href="http://www.ensembl.info/">
+                  <img alt="[RSS logo]" title="Ensembl blog" src="/i/rss_icon_16.png"></a>
+                  <a href="http://www.ensembl.info/">Blog</a></p>
+                <p><a class="media-icon" href="https://twitter.com/ensemblgenomes">
+                  <img alt="[twitter logo]" title="Follow us on Twitter!" src="/i/twitter.png"></a>
+                    <a href="https://twitter.com/ensemblgenomes">Twitter</a></p>
+              </div>
+  );
+
+  $html .= '</div>';
+
+  return $html;
+}
+
+1;
