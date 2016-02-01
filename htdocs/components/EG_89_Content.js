24ca6e1bb2c0e69ae1091367c434d2a1c868cb61
Sanjay Boddu <sboddu@ebi.ac.uk>
2016-02-01 10:54:55 +0000

    Revert ENSEMBL-4236 - Unhighlighting implemented - 6d81740c90c1b5c260debcea045eb2337009c245

--- htdocs/components/EG_89_Content.js
+++ htdocs/components/EG_89_Content.js
@@ -20,20 +20,9 @@ Ensembl.Panel.Content = Ensembl.Panel.Content.extend({
       Ensembl.updateURL({ ht: ht });
      
       panel.elLk.updateGeneTree.not(this).prop('checked', false);
-      panel.elLk.clearHighlightSelectors.prop('checked', false);
-
+     
       this.checked = true;
       Ensembl.EventManager.triggerSpecific('updatePanel', 'ComparaTree', undefined, null, { updateURL: Ensembl.updateURL({ ht: ht, collapse: collapse, update_panel: 1 }, this.value) }); // reload image
-    });
-
-    this.elLk.clearHighlightSelectors = $('input.clear_highlight_selectors', this.el).on('click', function () {
-      
-      Ensembl.historyReady = true;
-      Ensembl.updateURL({ ht: '' });
-      panel.elLk.updateGeneTree.prop('checked', false);
-
-      this.checked = true;
-      Ensembl.EventManager.triggerSpecific('updatePanel', 'ComparaTree', undefined, null, { updateURL: Ensembl.updateURL({ update_panel: 1 }, this.value) });
     });
  
     this.base();
