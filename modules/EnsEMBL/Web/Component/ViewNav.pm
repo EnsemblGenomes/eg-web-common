8efeb6757f02698782ea0b5392d8c4eedadf3fe6
Nick Langridge <nicklangridge@hotmail.com>
2016-04-20 10:31:39 +0100

    Change wording for webapollo link

--- modules/EnsEMBL/Web/Component/ViewNav.pm
+++ modules/EnsEMBL/Web/Component/ViewNav.pm
@@ -54,7 +54,7 @@ sub content {
     
     $annotation_link = sprintf(
       '<br /><a href="%s"><img src="/i/48/webapollo.png" title="Go to WebApollo to curate gene models" style="border:1px solid #ccc;margin:5px 8px 0px 8px;vertical-align:middle" /></a>
-      Go to <a href="%s">WebApollo</a> to curate gene models', 
+      Go to <a href="%s">WebApollo</a> to curate gene models (community annotation)', 
       $annotation_url,
       $annotation_url
     );
