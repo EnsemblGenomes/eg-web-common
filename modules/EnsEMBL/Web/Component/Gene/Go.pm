13f4b2a535472d2a15ee0f6e8df90e1fe99e82d1
Zhicheng-Liu <zl@ebi.ac.uk>
2016-04-27 15:07:02 +0100

    feat(Ontology): add "View associated genes" link to ontology table
    Previously in EG, where we have a chromosome, the ontology table offers a link to view xrefs on the karyotye, e.g.'View on karyotype'.
    
    This change offers a link ("View associated genes") to the whole genome view if we don't have a chromosome.
    
    Related to ENSEMBL-4466.

--- modules/EnsEMBL/Web/Component/Gene/Go.pm
+++ modules/EnsEMBL/Web/Component/Gene/Go.pm
@@ -112,17 +112,14 @@ sub process_data {
   my @row_styles;
 ##
 
-## EG
   my $chromosomes = $hub->species_defs->ENSEMBL_CHROMOSOMES;
-##
 
   foreach my $go (sort keys %$data) {
     my $hash        = $data->{$go} || {};
     my $go_link     = $hub->get_ExtURL_link($go, $extdb, $go);
     my $mart_link   = $self->biomart_link($go) ? "<li>".$self->biomart_link($go)."</li>": "";
-## EG: Only show the karyotype link for assembled genomes, otherwise this just links to a page with an error
-    my $loc_link    = $chromosomes && scalar @$chromosomes && $hub->species_defs->MAX_CHR_LENGTH ? '<li><a rel="notexternal" href="'.$hub->url({type  => 'Location', action => 'Genome', ftype => 'Gene', id  => $go}).'">View on karyotype</a></li>' : '';
-##
+
+    my $loc_link    = '<li><a rel="notexternal" href="' . $hub->url({type  => 'Location', action => 'Genome', ftype => 'Gene', id  => $go}) . ( $chromosomes && scalar @$chromosomes && $hub->species_defs->MAX_CHR_LENGTH ? '">View on karyotype</a></li>' : '">View associated genes</a></li>' );
 
     my $goslim      = $hash->{'goslim'} || {};
     my $row         = {};
