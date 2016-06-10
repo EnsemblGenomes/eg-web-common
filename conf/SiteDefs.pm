07ae9fc100f5a516589a7f3027ecff5029385358
Nick Langridge <nicklangridge@hotmail.com>
2016-06-10 15:08:32 +0100

    Version ++

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -26,8 +26,8 @@ sub update_conf {
 
   $SiteDefs::ENSEMBL_COHORT = 'EnsemblGenomes';
   
-  $SiteDefs::SITE_RELEASE_VERSION = 31;
-  $SiteDefs::SITE_RELEASE_DATE    = 'March 2015';
+  $SiteDefs::SITE_RELEASE_VERSION = 32;
+  $SiteDefs::SITE_RELEASE_DATE    = 'August 2015';
   $SiteDefs::SITE_MISSION         = 'Ensembl Genomes provides integrated access to genome-scale data from invertebrate metazoa, plants, fungi, protists and bacteria in partnership with the scientifc communities that work in each domain.';
     
   @SiteDefs::ENSEMBL_PERL_DIRS    = (
