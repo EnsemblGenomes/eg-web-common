ac8d177298e259dae6666146c571c6a21b821aa0
Nick Langridge <nicklangridge@hotmail.com>
2016-07-07 13:17:43 +0100

    Update to E85

--- modules/EnsEMBL/Web/Component/Gene/Go.pm
+++ modules/EnsEMBL/Web/Component/Gene/Go.pm
@@ -147,7 +147,7 @@ sub process_data {
       my $url        = $hub->url({
         species     => $species,
         type        => 'Gene',
-        action      => $type eq 'translation' ? 'Ontology' : 'Summary',
+        action      => $type eq 'translation' ? 'Ontologies/'.$hub->function : 'Summary',
         $param_type => $gene,
         __clear     => 1,
       });
