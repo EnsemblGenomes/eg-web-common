87b49e7e9ab0c1c8b9ece646e07771c542ecf0fa
Nick Langridge <nicklangridge@hotmail.com>
2016-07-08 09:50:33 +0100

    Update to E85 and remove code that is no longer needed

--- modules/EnsEMBL/Web/Object/Export.pm
+++ modules/EnsEMBL/Web/Object/Export.pm
@@ -1,6 +1,7 @@
 =head1 LICENSE
 
-Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,248 +19,37 @@ limitations under the License.
 
 package EnsEMBL::Web::Object::Export;
 
-sub config {
-  my $self = shift;
-  
-  $self->__data->{'config'} = {
-    fasta => {
-      label => 'FASTA sequence',
-      formats => [
-        [ 'fasta', 'FASTA sequence' ]
-      ],
-      params => [
-        [ 'cdna',    'cDNA' ],
-        [ 'coding',  'Coding sequence' ],
-        [ 'peptide', 'Peptide sequence' ],
-        [ 'utr5',    "5' UTR" ],
-        [ 'utr3',    "3' UTR" ],
-        [ 'exon',    'Exons' ],
-        [ 'intron',  'Introns' ]
-      ]
-    },
-    features => {
-      label => 'Feature File',
-      formats => [
-        [ 'csv',  'CSV (Comma separated values)' ],
-        [ 'tab',  'Tab separated values' ],
-        [ 'gtf',  'GTF (Gene Transfer Format)' ],
-        [ 'gff',  'GFF (Generic Feature Format)' ],
-        [ 'gff3', 'GFF3 (Generic Feature Format Version 3)' ],
-      ],
-      params => [
-        [ 'similarity', 'Similarity features' ],
-        [ 'repeat',     'Repeat features' ],
-        [ 'genscan',    'Prediction features (genscan)' ],
-        [ 'variation',  'Variation features' ],
-        [ 'probe',      'Probe features' ],
-        [ 'gene',       'Gene information' ],
-        [ 'transcript', 'Transcripts' ],
-        [ 'exon',       'Exons' ],
-        [ 'intron',     'Introns' ],
-        [ 'cds',        'Coding sequences' ]
-      ]
-    },
-    bed => {
-      label => 'Bed Format',
-      formats => [
-        [ 'bed',  'BED Format' ],
-      ],
-      params => [
-        [ 'variation',  'Variation features' ],
-        [ 'probe',      'Probe features' ],
-        [ 'gene',       'Gene information' ],
-        [ 'repeat',     'Repeat features' ],
-        [ 'similarity', 'Similarity features' ],
-        [ 'genscan',    'Prediction features (genscan)' ],
-        [ 'userdata',  'Uploaded Data' ],
-      ]
-    },
-#     PSL => {
-#       label => 'PSL Format',
-#       formats => [
-#         [ 'PSL',  'PSL Format' ],
-#       ],
-#       params => [
-#           [ 'name',  'Bed Line Name' ],
-#         ]
-#     },
-
-	flat => {
-      label => 'Flat File',
-      formats => [
-        [ 'embl',    'EMBL' ],
-        [ 'genbank', 'GenBank' ]
-      ],
-      params => [
-        [ 'similarity', 'Similarity features' ],
-        [ 'repeat',     'Repeat features' ],
-        [ 'genscan',    'Prediction features (genscan)' ],
-        [ 'contig',     'Contig Information' ],
-        [ 'variation',  'Variation features' ],
-        [ 'marker',     'Marker features' ],
-        [ 'gene',       'Gene Information' ],
-        [ 'vegagene',   'Vega Gene Information' ],
-        [ 'estgene',    'EST Gene Information' ]
-      ]
-    },
-
-    pip => {
-      label => 'PIP (%age identity plot)',
-      formats => [
-        [ 'pipmaker', 'Pipmaker / zPicture format' ],
-        [ 'vista',    'Vista Format' ]
-      ]
-    },
-    genetree => {
-      label => 'Gene Tree',
-      formats => [
-        [ 'phyloxml',    'PhyloXML from Compara' ],
-        [ 'phylopan',    'PhyloXML from Pan-taxonomic Compara' ]
-      ],
-      params => [
-        [ 'cdna', 'cDNA rather than protein sequence' ],
-        [ 'aligned', 'Aligned sequences with gaps' ],
-        [ 'no_sequences', 'Omit sequences' ],
-      ]
-    },
-    homologies => {
-      label => 'Homologies',
-      formats => [
-        [ 'orthoxml',    'OrthoXML from Compara' ],
-        [ 'orthopan',    'OrthoXML from Pan-taxonomic Compara' ]
-      ],
-      params => [
-        [ 'possible_orthologs', 'Treat not supported duplications as speciations (makes a non species-tree-compliant tree)' ],
-      ]
-    }  
-  };
-
-  if(! $self->get_object->can('get_GeneTree') ){
-	delete $self->__data->{'config'}{'genetree'};
-	delete $self->__data->{'config'}{'homologies'};
-  }
-  
-  my $func = sprintf 'modify_%s_options', lc $self->function;
-  $self->$func if $self->can($func);
-  
-  return $self->__data->{'config'};
-}
-
-sub features {
-  my $self = shift;
-  my $format = shift;
-  my $slice = $self->slice;
-  my $params = $self->params;
-  my @common_fields = qw(seqname source feature start end score strand frame);
-  my @extra_fields = $format eq 'gtf' ? qw(gene_id transcript_id) : qw(hid hstart hend genscan gene_id transcript_id exon_id gene_type variation_name probe_name);
-  my $availability = $self->availability;
-  
-  $self->{'config'} = {
-    extra_fields => \@extra_fields,
-    format => $format,
-    delim => $format eq 'csv' ? ',' : "\t"
-  };
-  
-  if($format ne 'bed'){$self->string(join $self->{'config'}->{'delim'}, @common_fields, @extra_fields) unless $format eq 'gff';}
-  
-  if ($params->{'similarity'}) {
-    foreach (@{$slice->get_all_SimilarityFeatures}) {
-      $self->feature('similarity', $_, {
-        hid => $_->hseqname,
-        hstart => $_->hstart,
-        hend => $_->hend
-      });
-    }
-  }
-  
-  if ($params->{'repeat'}) {
-    foreach (@{$slice->get_all_RepeatFeatures}) {
-      $self->feature('repeat', $_, {
-        hid => $_->repeat_consensus->name,
-        hstart => $_->hstart,
-        hend => $_->hend
-      });
-    }
-  }
-  
-  if ($params->{'genscan'}) {
-    foreach my $t (@{$slice->get_all_PredictionTranscripts}) {
-      foreach my $e (@{$t->get_all_Exons}) {
-        $self->feature('pred.trans.', $e, { genscan => $t->stable_id });
-      }
-    }
-  }
-  
-  if ($params->{'variation'}) {
-    foreach (@{$slice->get_all_VariationFeatures}) {
-      $self->feature('variation', $_, { variation_name => $_->variation_name });	
-    }
-  }
-
-  if($params->{'probe'} && $availability->{'database:funcgen'}) {
-    my $fg_db = $self->database('funcgen');
-## EG
-    my $probe_feature_adaptor = $fg_db ? $fg_db->get_ProbeFeatureAdaptor : undef;
-    my @probe_features = $probe_feature_adaptor ? @{$probe_feature_adaptor->fetch_all_by_Slice($slice)} : ();
-##
-    foreach my $pf(@probe_features){
-      my $probe_details = $pf->probe->get_all_complete_names();
-      my @probes = split(/:/,@$probe_details[0]);
-      $self->feature('ProbeFeature', $pf, { probe_name => @probes[1] },{ source => @probes[0]});
-    }
-  }
-  
-if ($params->{'gene'}) {
-    my $dbs = $self->dbs;
-    foreach my $db (@{$dbs}) {
-      foreach my $g (@{$slice->get_all_Genes(undef, $db)}) {
-        my $source = $self->gene_source($g,$db);
-        foreach my $t (@{$g->get_all_Transcripts}) {
-          foreach my $e (@{$t->get_all_Exons}) {
-            $self->feature('gene', $e, {
-               exon_id => $e->stable_id,
-               transcript_id => $t->stable_id,
-               gene_id => $g->stable_id,
-               gene_type => $g->status . '_' . $g->biotype
-            }, { source => $source });
-          }
-        }
-      }
-    }
-  }
- 
-  $self->misc_sets(keys %{$params->{'misc_set'}}) if $params->{'misc_set'};
-}
+use strict;
 
 sub gff3_features {
-  my $self = shift;
-  my $slice = $self->slice;
-  my $params = $self->params;
+  my $self         = shift;
+  my $slice        = $self->slice;
+  my $params       = $self->params;
   my $species_defs = $self->hub->species_defs;
   
   # Always use the forward strand, else CDS coordinates are incorrect (Bio::EnsEMBL::Exon->coding_region_start and _end return coords for forward strand only. Thanks, Core API team.)
   $slice = $slice->invert if $slice->strand < 0;
   
   $self->{'config'} = {
-    format => 'gff3',
-    delim => "\t",
+    format             => 'gff3',
+    delim              => "\t",
     ordered_attributes => {},
-    feature_order => {},
+    feature_order      => {},
     feature_type_count => 0,
     
     # TODO: feature types
-    # feature_map => {
-    # dna_align => { func => 'get_all_DnaAlignFeatures', type => 'nucleotide_match' },
-    # marker => { func => 'get_all_MarkerFeatures', type => 'region' },
-    # repeat => { func => 'get_all_RepeatFeatures', type => 'repeat_region' },
-    # assembly_exception => { func => 'get_all_AssemblyExceptionFeatures', type => '' },
-    # ditag => { func => 'get_all_DitagFeatures', type => '' },
-    # external => { func => 'get_all_ExternalFeatures', type => '' },
-    # oligo => { func => 'get_all_OligoFeatures', type => 'oligo' },
-    # qtl => { func => 'get_all_QtlFeatures', type => 'region' },
-    # simple => { func => 'get_all_SimpleFeatures', type => '' },
-    # protein_align => { func => 'get_all_ProteinAlignFeatures', type => 'protein_match' }
-    # }
+    #    feature_map => {
+    #      dna_align          => { func => 'get_all_DnaAlignFeatures',          type => 'nucleotide_match' },
+    #      marker             => { func => 'get_all_MarkerFeatures',            type => 'region' },
+    #      repeat             => { func => 'get_all_RepeatFeatures',            type => 'repeat_region' },
+    #      assembly_exception => { func => 'get_all_AssemblyExceptionFeatures', type => '' },
+    #      ditag              => { func => 'get_all_DitagFeatures',             type => '' },
+    #      external           => { func => 'get_all_ExternalFeatures',          type => '' },
+    #      oligo              => { func => 'get_all_OligoFeatures',             type => 'oligo' },
+    #      qtl                => { func => 'get_all_QtlFeatures',               type => 'region' },
+    #      simple             => { func => 'get_all_SimpleFeatures',            type => '' },
+    #      protein_align      => { func => 'get_all_ProteinAlignFeatures',      type => 'protein_match' }
+    #    }
   };
 
   my ($g_id, $t_id);
@@ -293,14 +83,14 @@ sub gff3_features {
         }
 
         if ($params->{'exon'} || $params->{'cds'}) {
+          foreach my $cds (@{$t->get_all_CDS||[]}) {
+            $self->feature('CDS', $cds, { Parent => $t_id, Name => $t->translation->stable_id }, $properties);
+          }
+        }
+
+        if ($params->{'exon'}) {
           foreach my $e (@{$t->get_all_Exons}) {
-            $self->feature('exon', $e, { Parent => $t_id, Name => $e->stable_id }, $properties) if $params->{'exon'};
- 
-            if ($params->{'cds'}) {
-              my $start = $e->coding_region_start($t);
-              next unless $start; # $start will be undef if the exon is not coding
-              $self->feature('CDS', $e, { Parent => $t_id, Name => $t->translation->stable_id }, $properties);
-            }
+            $self->feature('exon', $e, { Parent => $t_id, Name => $e->stable_id }, $properties);
           }
         }
       }
