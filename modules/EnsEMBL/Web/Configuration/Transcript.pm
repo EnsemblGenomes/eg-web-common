9d154840c43715c24f3cec56c7b0d05115ab36b0
Nick Langridge <nicklangridge@hotmail.com>
2016-10-03 10:55:35 +0100

    Remove zoomable transcript variation image - no longer supported

--- modules/EnsEMBL/Web/Configuration/Transcript.pm
+++ modules/EnsEMBL/Web/Configuration/Transcript.pm
@@ -23,36 +23,8 @@ package EnsEMBL::Web::Configuration::Transcript;
 use strict;
 use Data::Dumper;
 
-use base qw(EnsEMBL::Web::Configuration);
-
-# either - prediction transcript or transcript
-# domain - domain only (no transcript)
-# history - IDHistory object or transcript
-# database:variation - Variation database
-
 sub modify_tree {
   my $self = shift;
-  my $hub = $self->hub;
-  my $object = $self->object;
-  my $species_defs = $hub->species_defs;
-  my $protein_variations = $self->get_node('ProtVariations');  
-  
-  # Zoomable variation image
-
-  # $var_menu->append($self->create_node('Variation_Transcript/Image', 'Variation image',
-  #   [qw( variationimage EnsEMBL::Web::Component::Transcript::VariationImage )],
-  #   { 'availability' => 'transcript database:variation core' }
-  # ));
-
-  my $variation_image = $self->get_node('Variation_Transcript/Image');
-  
-  $variation_image->set('components', [qw( 
-    imagetop EnsEMBL::Web::Component::Transcript::VariationImageTop
-    imagenav EnsEMBL::Web::Component::Transcript::VariationImageNav
-    image EnsEMBL::Web::Component::Transcript::VariationImage 
-  )]);
-  
-  $variation_image->set('availability', 'transcript database:variation core');
   
 # EG:ENSEMBL-2785 add this new URL so that the Transcript info appears at the top of the page for the Karyotype display with Locations tables
   my $sim_node = $self->get_node('Similarity');
