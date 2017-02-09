ac4c1f1235e443337392f3436f386b26789d8f6e
Zhicheng-Liu <zl@ebi.ac.uk>
2017-02-09 10:47:42 +0000

    Add default setting for MAX_PROCESS_SIZE
    Currently set to 850000KB which is used for Plants, Protists and Metazoa.
    
    Related to ENSEMBL-4763.

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -42,6 +42,7 @@ sub update_conf {
 
   $SiteDefs::APACHE_BIN    = '/usr/sbin/httpd';
   $SiteDefs::APACHE_DIR    = '/etc/httpd';
+  $SiteDefs::MAX_PROCESS_SIZE = 850000; # Kill httpd over 850000KB
   $SiteDefs::SAMTOOLS_DIR  = '/nfs/public/rw/ensembl/samtools';
 
   # Does this site have a large species set?
