34c3ef7d8f1fd24bfa7fc19a8eb4f25102f52030
Nick Langridge <nicklangridge@hotmail.com>
2017-03-29 17:06:26 +0100

    Don't build spritemaps for EG

---
+++ modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
@@ -0,0 +1,54 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Tools::DHTMLmerge;
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Exceptions;
+
+sub merge_all {
+  ## This merges all CSS and JS files and saves the combined and minified ones on the disk
+  ## Call this at startup
+  ## @param EnsEMBL::Web::SpeciesDefs object
+  my $species_defs  = shift;
+  my $configs       = {};
+
+  try {
+## EG - don't merge images
+    #foreach my $type (qw(js css ie7css image)) {
+    foreach my $type (qw(js css ie7css)) {
+##      
+      push @{$configs->{$type}}, grep { !$_->empty } map { EnsEMBL::Web::Tools::DHTMLmerge::FileGroup->new($species_defs, $type, $_) } get_filegroups($species_defs, $type);
+    }
+    for (@{$configs->{'image'}}) {
+      delete $_->{'files'};
+    }
+    $species_defs->set_config('ENSEMBL_JSCSS_FILES', $configs);
+    $species_defs->store;
+  } catch {
+    warn $_;
+    throw $_;
+  };
+}
+
+
+
+1;
