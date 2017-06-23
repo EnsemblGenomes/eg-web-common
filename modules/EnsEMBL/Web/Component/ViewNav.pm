77ffcabf6b4e4b9c862164f289462368f6b1542b
Sanjay Boddu <sboddu@ebi.ac.uk>
2017-06-23 12:07:58 +0100

    VB-6523 - Enable link to WebApollo but leave link to Polyploid view disabled
    Link to WebApollo disabled by mistake while disabling link to Polyploid in the past - 07ca60c353effe775b403a3cf6e5402a6fa670da

--- modules/EnsEMBL/Web/Component/ViewNav.pm
+++ modules/EnsEMBL/Web/Component/ViewNav.pm
@@ -21,12 +21,12 @@ package EnsEMBL::Web::Component::ViewNav;
 use strict;
 
 ## ENSEMBL-4620 / ENSEMBL-4718 polyploid view currently disabled
-# sub content {
-#   my $self        = shift;
-#   my $hub         = $self->hub;
-#   my $image_width = $self->image_width . 'px';
-#   my $r           = $hub->create_padded_region()->{'r'} || $hub->param('r');
-#   my $url         = $hub->url({'type' => 'Location', 'action' => 'View', 'r' => $r});
+ sub content {
+   my $self        = shift;
+   my $hub         = $self->hub;
+   my $image_width = $self->image_width . 'px';
+   my $r           = $hub->create_padded_region()->{'r'} || $hub->param('r');
+   my $url         = $hub->url({'type' => 'Location', 'action' => 'View', 'r' => $r});
 
 # ## EG  
 #   my $polyploid_link = '';
@@ -37,30 +37,28 @@ use strict;
 #     );
 #   }
 
-#   my $annotation_link = '';
-#   if (my $annotation_url = $hub->species_defs->ANNOTATION_URL) {
-#     my $object = $self->object;
-#     my ($sr, $start, $end) = ($object->seq_region_name, $object->seq_region_start, $object->seq_region_end);
-#     $annotation_url =~ s/###SEQ_REGION###/$sr/;
-#     $annotation_url =~ s/###START###/$start/;
-#     $annotation_url =~ s/###END###/$end/;
-    
-#     $annotation_link = sprintf(
-#       '<br /><a href="%s"><img src="/i/48/webapollo.png" title="Go to WebApollo to curate gene models" style="border:1px solid #ccc;margin:5px 8px 0px 8px;vertical-align:middle" /></a>
-#       Go to <a href="%s">WebApollo</a> to curate gene models (community annotation)', 
-#       $annotation_url,
-#       $annotation_url
-#     );
-#   }
+   my $annotation_link = '';
+   if (my $annotation_url = $hub->species_defs->ANNOTATION_URL) {
+     my $object = $self->object;
+     my ($sr, $start, $end) = ($object->seq_region_name, $object->seq_region_start, $object->seq_region_end);
+     $annotation_url =~ s/###SEQ_REGION###/$sr/;
+     $annotation_url =~ s/###START###/$start/;
+     $annotation_url =~ s/###END###/$end/;
+   
+     $annotation_link = sprintf(
+       '<br /><a href="%s"><img src="/i/48/webapollo.png" title="Go to WebApollo to curate gene models" style="border:1px solid #ccc;margin:5px 8px 0px 8px;vertical-align:middle" /></a>
+       Go to <a href="%s">WebApollo</a> to curate gene models (community annotation)', 
+       $annotation_url,
+       $annotation_url
+     );
+   }
 
-#   return qq{
-#       <div class="navbar print_hide" style="width:$image_width">
-#         <a href="$url"><img src="/i/48/region_thumb.png" title="Go to Region in Detail for more options" style="border:1px solid #ccc;margin:0 8px;vertical-align:middle" /></a> Go to <a href="$url" class="no-visit">Region in Detail</a> for more tracks and navigation options (e.g. zooming)
-#         $annotation_link
-#       </div>
-#       $polyploid_link 
-#   };
-# ##  
-# }
+   return qq{
+       <div class="navbar print_hide" style="width:$image_width">
+         <a href="$url"><img src="/i/48/region_thumb.png" title="Go to Region in Detail for more options" style="border:1px solid #ccc;margin:0 8px;vertical-align:middle" /></a> Go to <a href="$url" class="no-visit">Region in Detail</a> for more tracks and navigation options (e.g. zooming)
+         $annotation_link
+       </div>
+   };
+ }
 
 1;
