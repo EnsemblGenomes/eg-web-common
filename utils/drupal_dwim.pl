4cfba845e2739ca23019447559ec692e896b8863
Dan Sheppard <dan@ebi.ac.uk>
2018-01-04 17:33:00 +0000

    Use .sconf files to automate version, division updating drupal site.

---
+++ utils/drupal_dwim.pl
@@ -0,0 +1,25 @@
+#! /usr/bin/env perl
+
+# Update the drupals in this checkout per SOP.
+
+use strict;
+use warnings;
+
+use FindBin qw($Bin);
+use Storable;
+
+my $conf = retrieve("$Bin/../../config.sconf");
+
+my $division = $conf->{'SiteDefs'}{'DIVISION'};
+my $release = $conf->{'SiteDefs'}{'SITE_RELEASE_VERSION'};
+
+warn "updating drupal for $division/$release\n";
+
+system("$Bin/drupal_import_home.pl -d $division -r $release") && die;
+unless($division eq 'bacteria') {
+  system("$Bin/drupal_import_species.pl -d $division") && die;
+}
+
+1;
+
+
