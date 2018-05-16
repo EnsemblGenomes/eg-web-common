c103492c5ed8da3ffe04de1bcd5eb98e00b111fe
Steve Trevanion <ens-st3@users.noreply.github.com>
2018-05-16 14:08:40 +0100

    Update SiteDefs.pm
    Ready for eg40

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -26,8 +26,8 @@ sub update_conf {
 
   $SiteDefs::ENSEMBL_COHORT = 'EnsemblGenomes';
   
-  $SiteDefs::SITE_RELEASE_VERSION = 39;
-  $SiteDefs::SITE_RELEASE_DATE    = 'April 2018';
+  $SiteDefs::SITE_RELEASE_VERSION = 40;
+  $SiteDefs::SITE_RELEASE_DATE    = 'July 2018';
   $SiteDefs::SITE_MISSION         = 'Ensembl Genomes provides integrated access to genome-scale data from invertebrate metazoa, plants, fungi, protists and bacteria in partnership with the scientifc communities that work in each domain.';
     
   @SiteDefs::ENSEMBL_PERL_DIRS    = (
