b9778d2cc85ee8dbae3e1248883091c3b673005a
Nick Langridge <nicklangridge@hotmail.com>
2019-06-25 15:16:00 +0100

    Don't include division in BLAST prefix variable

--- modules/EnsEMBL/Web/SpeciesDefs.pm
+++ modules/EnsEMBL/Web/SpeciesDefs.pm
@@ -33,7 +33,7 @@ sub _get_NCBIBLAST_source_file {
   (my $type = lc $source_type) =~ s/_/\./;
 
   my $unit = $self->GENOMIC_UNIT;
-  my $path = $self->EBI_BLAST_DB_PREFIX || "ensemblgenomes/$unit";
+  my $path = ($self->EBI_BLAST_DB_PREFIX || "ensemblgenomes") . "/$unit";
   
   my $dataset = $self->get_config($species, 'SPECIES_DATASET');
 
