f7a141da209e9f404a5830ddd2227e2619c6669f
Steve Trevanion <steve@ebi.ac.uk>
2021-02-03 11:30:20 +0000

    version++

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -26,8 +26,8 @@ sub update_conf {
 
   $SiteDefs::ENSEMBL_COHORT = 'EnsemblGenomes';
   
-  $SiteDefs::SITE_RELEASE_VERSION = 50;
-  $SiteDefs::SITE_RELEASE_DATE    = 'February 2021';
+  $SiteDefs::SITE_RELEASE_VERSION = 51;
+  $SiteDefs::SITE_RELEASE_DATE    = 'April 2021';
   $SiteDefs::SITE_MISSION         = 'Ensembl Genomes provides integrated access to genome-scale data from invertebrate metazoa, plants, fungi, protists and bacteria in partnership with the scientifc communities that work in each domain.';
   $SiteDefs::BIOSCHEMAS_DATACATALOG = defer { 'http://'.$SiteDefs::DIVISION.'.ensembl.org/#project' };
     
