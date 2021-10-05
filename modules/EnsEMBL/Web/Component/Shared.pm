218ac5dde6ed1f39a3ab9c3208544be31a8f327c
Andrey Azov <andrey@ebi.ac.uk>
2021-10-05 23:11:03 +0000

    Update ENA link for assemblies

--- modules/EnsEMBL/Web/Component/Shared.pm
+++ modules/EnsEMBL/Web/Component/Shared.pm
@@ -618,7 +618,7 @@ sub species_stats {
       $a_id .= " ($long)";
     }
     if (my ($acc) = @{$meta_container->list_value_by_key('assembly.accession')}) {
-      $acc = sprintf('INSDC Assembly <a href="http://www.ebi.ac.uk/ena/data/view/%s">%s</a>', $acc, $acc);
+      $acc = sprintf('INSDC Assembly <a href="https://www.ebi.ac.uk/ena/browser/view/%s">%s</a>', $acc, $acc);
       $a_id .= ", $acc";
     }
   }
