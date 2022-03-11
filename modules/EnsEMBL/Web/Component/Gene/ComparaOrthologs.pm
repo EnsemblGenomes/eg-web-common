872583a12f627389516bdde8f4094838802a56c2
Anne Lyle <annelyle@ebi.ac.uk>
2022-03-11 16:07:27 +0000

    Remove duplicate code

--- modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -20,101 +20,27 @@ package EnsEMBL::Web::Component::Gene::ComparaOrthologs;
 
 use strict;
 
-sub _species_sets {
-## Group species into sets - separate method so it can be pluggable easily
-  my ($self, $orthologue_list, $orthologue_map, $cdb) = @_;
-  
-  my $hub             = $self->hub;
-  my $species_defs    = $self->hub->species_defs;
-  my $lookup          = {}; 
-  my $compara_spp     = {};
-  my $sets_by_species = {};
+sub _species_set_config {
+  my ($self, $cdb) = @_;
   my $set_order       = [];
+  my $species_sets    = {};
   my $is_pan          = $cdb =~/compara_pan_ensembl/;
-  my $pan_info        = $is_pan ? $species_defs->multi_val('PAN_COMPARA_LOOKUP') : {};
 
   if ($is_pan) {
     $set_order    = [qw(all vertebrates metazoa plants fungi protists bacteria archaea)];
-    foreach (keys %$pan_info) {
-      $compara_spp->{$_}  = 1;
-      $lookup->{$_}       = $pan_info->{$_}{'species_url'};
-    }
+    $species_sets = {
+      'vertebrates' => {'title' => 'Vertebrates', 'desc' => '', 'species' => [], 'all' => 0},
+      'metazoa'     => {'title' => 'Metazoa',     'desc' => '', 'species' => [], 'all' => 0},
+      'plants'      => {'title' => 'Plants',      'desc' => '', 'species' => [], 'all' => 0},
+      'fungi'       => {'title' => 'Fungi',       'desc' => '', 'species' => [], 'all' => 0},
+      'protists'    => {'title' => 'Protists',    'desc' => '', 'species' => [], 'all' => 0},
+      'bacteria'    => {'title' => 'Bacteria',    'desc' => '', 'species' => [], 'all' => 0},
+      'archaea'     => {'title' => 'Archaea',     'desc' => '', 'species' => [], 'all' => 0},
+      'all'         => {'title' => 'All',         'desc' => '', 'species' => [], 'all' => 0},
+    };
   }
-  else {
-    $compara_spp  = $species_defs->multi_hash->{'DATABASE_COMPARA'}{'COMPARA_SPECIES'};
-    $lookup       = $hub->species_defs->prodnames_to_urls_lookup;
-  }
-  
-  my $species_sets = $is_pan ? {
-    'vertebrates' => {'title' => 'Vertebrates', 'desc' => '', 'species' => [], 'all' => 0},
-    'metazoa'     => {'title' => 'Metazoa',     'desc' => '', 'species' => [], 'all' => 0},
-    'plants'      => {'title' => 'Plants',      'desc' => '', 'species' => [], 'all' => 0},
-    'fungi'       => {'title' => 'Fungi',       'desc' => '', 'species' => [], 'all' => 0},
-    'protists'    => {'title' => 'Protists',    'desc' => '', 'species' => [], 'all' => 0},
-    'bacteria'    => {'title' => 'Bacteria',    'desc' => '', 'species' => [], 'all' => 0},
-    'archaea'     => {'title' => 'Archaea',     'desc' => '', 'species' => [], 'all' => 0},
-    'all'         => {'title' => 'All',         'desc' => '', 'species' => [], 'all' => 0},
-  } : {};
-  
-  foreach my $prod_name (keys %$compara_spp) {
-
-    my $species = $lookup->{$prod_name};
-    my $orthologues = $orthologue_list->{$species};
-    my $no_ortho = 0;
-    my ($orth_type, $group);
-    my $sets = [];
-
-    if (!$orthologue_list->{$species} && $species ne $self->hub->species) {
-      $no_ortho = 1;
-    }
-
-    foreach my $stable_id (keys %$orthologues) {
-      my $orth_info = $orthologue_list->{$species}->{$stable_id};
-      my $orth_desc = ucfirst($orth_info->{'homology_desc'});
-      $orth_type->{$species}{$orth_desc} = 1;
-    }
-
-    if ($species ne $self->hub->species && !$orth_type->{$species}{'1-to-1'} && !$orth_type->{$species}{'1-to-many'}
-          && !$orth_type->{$species}{'Many-to-many'}) {
-      $no_ortho = 1;
-    }
-
-    ## Sort into groups
-    if ($is_pan) {
-      $group = $pan_info->{$prod_name}{'subdivision'} ? $pan_info->{$prod_name}{'subdivision'} : $pan_info->{$prod_name}{'division'};
-    }
-    else {
-      # not the pan compara page - generate groups
-      $group = $species_defs->get_config($species, 'SPECIES_GROUP') || 'Undefined';
-
-      if(!exists $species_sets->{$group}){
-        $species_sets->{$group} = {'title' => ucfirst $group, 'species' => [], 'all' => 0};
-        push @$set_order, $group;
-      }
-    }    
-
-    foreach my $ss_name ('all', $group) {
-      push @{$species_sets->{$ss_name}{'species'}}, $species;
-      push (@$sets, $ss_name) if exists $species_sets->{$ss_name};
-
-      while (my ($k, $v) = each (%{$orth_type->{$species}})) {
-        $species_sets->{$ss_name}{$k} += $v;
-      }
-
-      $species_sets->{$ss_name}{'none'}++ if $no_ortho;
-      $species_sets->{$ss_name}{'all'}++ if $species ne $self->hub->species;
-    }
-    
-    $sets_by_species->{$species} = $sets;
-  }
-
-  if(!$is_pan) {
-    my @unorder = @$set_order;
-    @$set_order = sort(@unorder);
-    unshift(@$set_order, 'all');
-  }
-
-  return ($species_sets, $sets_by_species, $set_order);
+  return ($set_order, $species_sets);
 }
 
+  
 1;
