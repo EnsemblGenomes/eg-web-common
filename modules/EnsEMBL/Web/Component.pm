04a9dd24bcc2585e4a95043b6650be62b678c560
Anne Lyle <annelyle@ebi.ac.uk>
2022-04-12 15:13:05 +0100

    Simple wrappers for pan-compara content - ENSWEB-6600

---
+++ modules/EnsEMBL/Web/Component.pm
@@ -0,0 +1,53 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2022] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Component;
+
+sub content_pan_compara {
+  my $self = shift;
+  return $self->content('compara_pan_ensembl');
+}
+
+sub content_text_pan_compara {
+  my $self = shift;
+  return $self->content_text('compara_pan_ensembl');
+}
+
+sub content_align_pan_compara {
+  my $self = shift;
+  return $self->content_align('compara_pan_ensembl');
+}
+
+sub content_alignment_pan_compara {
+  my $self = shift;
+  return $self->content('compara_pan_ensembl');
+}
+
+sub content_ensembl_pan_compara {
+  my $self = shift;
+  return $self->content_ensembl('compara_pan_ensembl');
+}
+
+sub content_other_pan_compara {
+  my $self = shift;
+  return $self->content_other('compara_pan_ensembl');
+}
+
+
+1;
