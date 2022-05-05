ec6dbc02d2aaf862268c17734af376d8870e5a22
Anne Lyle <annelyle@ebi.ac.uk>
2022-05-05 12:49:22 +0000

    Add species sets needed by orthologue summary table

--- modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -22,6 +22,9 @@ use strict;
 
 sub species_set_config {
   my ($self, $cdb) = @_;
+  my $species_defs = $self->hub->species_defs;
+
+  my @unsorted        = ();
   my $set_order       = [];
   my $species_sets    = {};
   my $is_pan          = $cdb =~/compara_pan_ensembl/;
@@ -29,15 +32,41 @@ sub species_set_config {
   if ($is_pan) {
     $set_order    = [qw(all vertebrates metazoa plants fungi protists bacteria archaea)];
     $species_sets = {
-      'vertebrates' => {'title' => 'Vertebrates', 'desc' => '', 'species' => [], 'all' => 0},
-      'metazoa'     => {'title' => 'Metazoa',     'desc' => '', 'species' => [], 'all' => 0},
-      'plants'      => {'title' => 'Plants',      'desc' => '', 'species' => [], 'all' => 0},
-      'fungi'       => {'title' => 'Fungi',       'desc' => '', 'species' => [], 'all' => 0},
-      'protists'    => {'title' => 'Protists',    'desc' => '', 'species' => [], 'all' => 0},
-      'bacteria'    => {'title' => 'Bacteria',    'desc' => '', 'species' => [], 'all' => 0},
-      'archaea'     => {'title' => 'Archaea',     'desc' => '', 'species' => [], 'all' => 0},
-      'all'         => {'title' => 'All',         'desc' => '', 'species' => [], 'all' => 0},
+      'vertebrates' => {'title' => 'Vertebrates'},
+      'metazoa'     => {'title' => 'Metazoa'},
+      'plants'      => {'title' => 'Plants'},
+      'fungi'       => {'title' => 'Fungi'},
+      'protists'    => {'title' => 'Protists'},
+      'bacteria'    => {'title' => 'Bacteria'},
+      'archaea'     => {'title' => 'Archaea'},
+      'all'         => {'title' => 'All'},
     };
+  }
+  else {
+    $species_sets = {
+      'all'         => {'title' => 'All'},
+    };
+
+    ## Work out the species sets from taxonomic groups
+
+    my $compara_spp = $species_defs->multi_hash->{'DATABASE_COMPARA'}{'COMPARA_SPECIES'};
+    my $lookup      = $species_defs->prodnames_to_urls_lookup;
+
+    foreach my $prod_name (keys %$compara_spp) {
+
+      next if $prod_name eq 'ancestral_sequences';
+      my $species = $lookup->{$prod_name};
+      next unless $species;
+      my $group   = $species_defs->get_config($species, 'SPECIES_GROUP') || 'Undefined';
+
+      if (!exists $species_sets->{$group}){
+        $species_sets->{$group} = {'title' => ucfirst $group};
+        push @unsorted, $group;
+      }
+    }
+
+    @$set_order = sort (@unsorted);
+    unshift(@$set_order, 'all');
   }
   return ($set_order, $species_sets);
 }
