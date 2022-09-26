ff61ac444d548c3376ad304a1957237c8185c5dd
Andres Veidenberg <andres@ebi.ac.uk>
2022-09-26 22:50:32 +0300

    Update release date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -27,7 +27,7 @@ sub update_conf {
   $SiteDefs::ENSEMBL_COHORT = 'EnsemblGenomes';
   
   $SiteDefs::SITE_RELEASE_VERSION = 55;
-  $SiteDefs::SITE_RELEASE_DATE    = 'Sep 2022';
+  $SiteDefs::SITE_RELEASE_DATE    = 'Oct 2022';
   $SiteDefs::SITE_MISSION         = 'Ensembl Genomes provides integrated access to genome-scale data from invertebrate metazoa, plants, fungi, protists and bacteria in partnership with the scientifc communities that work in each domain.';
   $SiteDefs::BIOSCHEMAS_DATACATALOG = defer { 'http://'.$SiteDefs::DIVISION.'.ensembl.org/#project' };
     
