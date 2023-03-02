8ab04ec06b102b199d8fefe2f5b8f21eb9711990
John Tate <jgt@ebi.ac.uk>
2023-03-02 18:00:19 +0000

    Update version and date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -26,8 +26,8 @@ sub update_conf {
 
   $SiteDefs::ENSEMBL_COHORT = 'EnsemblGenomes';
   
-  $SiteDefs::SITE_RELEASE_VERSION = 56;
-  $SiteDefs::SITE_RELEASE_DATE    = 'Feb 2023';
+  $SiteDefs::SITE_RELEASE_VERSION = 57;
+  $SiteDefs::SITE_RELEASE_DATE    = 'May 2023';
   $SiteDefs::SITE_MISSION         = 'Ensembl Genomes provides integrated access to genome-scale data from invertebrate metazoa, plants, fungi, protists and bacteria in partnership with the scientifc communities that work in each domain.';
   $SiteDefs::BIOSCHEMAS_DATACATALOG = defer { 'http://'.$SiteDefs::DIVISION.'.ensembl.org/#project' };
     
