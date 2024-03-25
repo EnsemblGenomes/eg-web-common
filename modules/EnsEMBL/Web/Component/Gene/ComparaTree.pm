add6c790fc4be47df829311221a45b4f372fe6d5
Thomas Walsh <twalsh@ebi.ac.uk>
2024-03-25 04:55:23 +0000

    Fix highlighted gene ID in gene tree
    Co-authored-by: Imran Salam <32512909+imransl@users.noreply.github.com>

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -120,12 +120,18 @@ sub content {
 
   # store g1 param in a different param as $highlight_gene can be undef if highlighting is disabled
   my $gene_to_highlight = $hub->param('g1');
-  my $highlight_gene_display_label;
+  my $current_gene_display_name;
+
+  if ($gene && defined $gene->Obj->display_xref) {
+    $current_gene_display_name = $gene->Obj->display_xref->display_id;
+  } else {
+    $current_gene_display_name = $hub->param('g');
+  }
+
   my $lookup = $sd->prodnames_to_urls_lookup;
   
   foreach my $this_leaf (@$leaves) {
     if ($gene_to_highlight && $this_leaf->gene_member->stable_id eq $gene_to_highlight) {
-      $highlight_gene_display_label = $this_leaf->gene_member->display_label || $gene_to_highlight;
       $highlight_species            = $lookup->{$this_leaf->gene_member->genome_db->name};
       $highlight_species_name       = $this_leaf->gene_member->genome_db->display_name;
       $highlight_genome_db_id       = $this_leaf->gene_member->genome_db_id;
@@ -145,7 +151,7 @@ sub content {
           sprintf(
             '<p>The <i>%s</i> %s gene, its paralogues, its orthologue in <i>%s</i>, and paralogues of the <i>%s</i> gene, have all been highlighted. <a href="#" class="switch_highlighting on">Click here to disable highlighting</a>.</p>',
             $sd->get_config($lookup->{$member->genome_db->name}, 'SPECIES_DISPLAY_NAME'),
-            $highlight_gene_display_label,
+            $current_gene_display_name,
             $sd->get_config($highlight_species, 'SPECIES_DISPLAY_NAME') || $highlight_species_name,
             $sd->get_config($highlight_species, 'SPECIES_DISPLAY_NAME') || $highlight_species_name
           )
@@ -168,7 +174,7 @@ sub content {
         sprintf(
           '<p>The <i>%s</i> %s gene and its paralogues are highlighted. <a href="#" class="switch_highlighting off">Click here to enable highlighting of %s homologues</a>.</p>',
           $sd->get_config($lookup->{$member->genome_db->name}, 'SPECIES_DISPLAY_NAME'),
-          $highlight_gene_display_label,
+          $current_gene_display_name,
           $sd->get_config($highlight_species, 'SPECIES_DISPLAY_NAME') || $highlight_species_name
         )
       );
